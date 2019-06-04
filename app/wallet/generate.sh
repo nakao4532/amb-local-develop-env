25b148715ba67166854fec1dee8d652dae017ff8
nakao_kazuyoshi <nakao_kazuyoshi@cyberagent.co.jp>
2019-06-04 15:24:59 +0900

    Add wallet db in application development

---
+++ app/wallet/generate.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+CHANNEL_NAME=mychannel
+
+# remove previous crypto material and config transactions
+rm -fr config/*
+rm -fr crypto-config/*
+
+# setup cli container
+docker-compose up -d cli
+
+# generate crypto material
+docker-compose exec cli cryptogen generate --config=./crypto-config.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  docker-compose down
+  exit 1
+fi
+
+# generate genesis block for orderer
+docker-compose exec cli configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+docker-compose exec cli configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  docker-compose down
+  exit 1
+fi
+
+# generate anchor peer transaction
+docker-compose exec cli configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org1MSP..."
+  docker-compose down
+  exit 1
+fi
+
+docker-compose exec cli mv ./crypto-config/peerOrganizations/org1.example.com/ca/*_sk ./crypto-config/peerOrganizations/org1.example.com/ca/ca.org1.example.com-key.pem
+docker-compose down
